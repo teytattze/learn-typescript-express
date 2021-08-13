620802349599be85085b6db8a9ddf73cc37bfa70
tattzetey <tattzetey@gmail.com>
2021-08-14 01:33:22 +0800

    add example env file

--- src/lib/init-env.ts
+++ src/lib/init-env.ts
@@ -1,11 +1,12 @@
 import * as dotenv from 'dotenv';
 
-export type IEnvironment = 'development' | 'test' | 'production';
+export type IEnvironment = 'default' | 'development' | 'test' | 'production';
 
-export const initEnv = (environment: IEnvironment) =>
+export const initEnv = (environment: IEnvironment = 'default') =>
   dotenv.config({ path: pathMap[environment] });
 
 const pathMap: Record<NonNullable<IEnvironment>, string> = {
+  default: '.env',
   development: '.env.development',
   test: '.env.test',
   production: '.env.production',
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import { validateEnv } from './lib/validate-env';
 import { PostsController } from './modules/posts/posts.controller';
 import { App } from './app';
 
+// leave the params blank
+// if you want to use .env
 initEnv(process.env.NODE_ENV as IEnvironment);
 validateEnv();
 
